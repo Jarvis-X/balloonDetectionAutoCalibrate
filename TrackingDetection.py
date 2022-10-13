64626221695780ef6060b33bb901b3c3dc90214d
Jiawei Xu <cyandicnx@hotmail.com>
2022-10-12 22:16:38 -0400

    Add files via upload

---
+++ TrackingDetection.py
@@ -0,0 +1,17 @@
+class TrackingDetection:
+    """
+    The class to process the raw detection results from the camera and opencv
+    """
+    # TODO: populate this class to accommodate the raw balloon tracking results and provide proper filtering
+    def __init__(self):
+        pass
+
+    def _filter(self):
+        pass
+
+    def update(self, new_detections):
+        pass
+
+    def get(self):
+        pass
+
