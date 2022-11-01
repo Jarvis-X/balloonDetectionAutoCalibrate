4010a71369d40a738be465d819ecdaefb951df77
Karen Li <kkl224@lehigh.edu>
2022-11-01 17:15:58 -0400

    commit

--- main.py
+++ main.py
@@ -278,6 +278,7 @@ def getBallonContours(detector, frame, frameContour, ratio, bcxdata, bcydata, di
         # Get the number of blobs found
         p = [None] * len(box_width)
         print(len(box_width))
+        d = []
         for i in range(len(box_width)):
             p[i] = box_width[i]             # perceived width, in pixels
             w = BALLOONWIDTH         # approx. actual width, in meters (pre-computed)
